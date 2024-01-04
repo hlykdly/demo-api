8d908344627a9c73a63969aa9750abcc7273c92d
hlykdly <mayamoto@protonmail.com>
2024-01-04 17:04:00 +0100

    first commit

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+public class Person {
+
+    long id;
+    String name;
+    int age;
+    String email;
+
+    public Person(long id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/PersonController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/persons")
+public class PersonController {
+
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        // For simplicity, returning a static list. In a real-world application, this would come from a database.
+        return Arrays.asList(
+                new Person(1L, "John", 30, "john@email.com"),
+                new Person(2L, "Jane", 25, "jane@email.com"),
+                new Person(1L,"pete",43, "asd@asd.com")
+        );
+    }
+
+    @GetMapping("/pete")
+    public Person pete(){
+        return new Person(1L,"pete",43, "asd@asd.com");
+    }
+}
